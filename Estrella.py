fb86211a737006f0b852c65b3bea0244c087161d
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2021-12-16 18:16:25 +0100

    aplico la arcotangente para intentar hallar los lados del ángulo, por ahora no funciona

--- Estrella.py
+++ Estrella.py
@@ -1,15 +1,18 @@
 import turtle
+import math
 print("¿Cuántas puntas tiene tu estrella?")
 puntas=int(input())
 angulo= 360//puntas #/ división con decimales, // división sin decimales
 print(angulo)
-
+arcotangente=math.atan(angulo)
+distancialateral = 100/arcotangente
+print(distancialateral)
 estrella=turtle.Turtle()
 pantalla=turtle.Screen()
 pantalla.bgcolor("White")
 estrella.speed(1) #Tiempo que tarda en dibujar en escala de 1: muy lento, 10 muy rápido
 for i in range(puntas):
 
-    estrella.right(144)
+    estrella.right(distancialateral)
     estrella.forward(100)
 turtle.done()
