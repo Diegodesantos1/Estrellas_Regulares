240b9e860d55fed3788cb4a58417ed78a45a8ab6
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2021-12-16 18:43:41 +0100

    cambiados los colores

--- Estrella.py
+++ Estrella.py
@@ -5,8 +5,9 @@ angulo= (puntas - 2) * 180 / puntas #Fórmula del ángulo interno de un polígon
 angulo_positivo = (1 - 4/puntas) * 180 #Fórmula del ángulo interno positivo de un polígono
 estrella=turtle.Turtle()
 pantalla=turtle.Screen()
+turtle.pencolor("White")
 turtle.pensize(3) # Pongo el tamaño un poco más grueso
-pantalla.bgcolor("White")
+pantalla.bgcolor("Black")
 estrella.speed(1) #Tiempo que tarda en dibujar en escala de 1: muy lento, 10 muy rápido
 for i in range(puntas):
     turtle.forward(20)
