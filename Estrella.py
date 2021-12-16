5d5a007f791098cb6f139e2f36256bcdebcc60d4
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2021-12-16 18:07:01 +0100

    ahora la flecha comienza hacia la derecha

--- Estrella.py
+++ Estrella.py
@@ -1,14 +1,15 @@
 import turtle
 print("¿Cuántas puntas tiene tu estrella?")
 puntas=int(input())
-
-angulo= 360//puntas
+angulo= 360//puntas #/ división con decimales, // división sin decimales
 print(angulo)
+
 estrella=turtle.Turtle()
 pantalla=turtle.Screen()
 pantalla.bgcolor("White")
 estrella.speed(1) #Tiempo que tarda en dibujar en escala de 1: muy lento, 10 muy rápido
-for i in range(puntas): #Solo funciona para la estrella de 5 puntas
-    estrella.left(144)
+for i in range(puntas):
+
+    estrella.right(144)
     estrella.forward(100)
 turtle.done()
