d2d663a0f088f254971bcd88e4e53926d055a3bc
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2021-12-16 18:42:22 +0100

    Establecido el bucle que genera la estrella usando los ángulos previamente calculados

--- Estrella.py
+++ Estrella.py
@@ -1,5 +1,4 @@
 import turtle
-import math
 print("¿Cuántas puntas tiene tu estrella?")
 puntas=int(input())
 angulo= (puntas - 2) * 180 / puntas #Fórmula del ángulo interno de un polígono
@@ -9,5 +8,9 @@ pantalla=turtle.Screen()
 turtle.pensize(3) # Pongo el tamaño un poco más grueso
 pantalla.bgcolor("White")
 estrella.speed(1) #Tiempo que tarda en dibujar en escala de 1: muy lento, 10 muy rápido
-
+for i in range(puntas):
+    turtle.forward(20)
+    turtle.right(180 - angulo_positivo)
+    turtle.forward(20)
+    turtle.left(180 - angulo)
 turtle.done()
