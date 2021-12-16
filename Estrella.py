a03b2affd542c7b2c56c6fd2c8ff7a0cdd646575
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2021-12-16 17:56:28 +0100

    funciona con 5 puntos, falta aplicarlo a cualquer número de puntas

--- Estrella.py
+++ Estrella.py
@@ -3,5 +3,10 @@ print("¿Cuántas puntas tiene tu estrella?")
 puntas=int(input())
 estrella=turtle.Turtle()
 pantalla=turtle.Screen()
-pantalla.bgcolor("Black")
-estrella.speed(20) #Tiempo que tarda en dibujar
+pantalla.bgcolor("White")
+estrella.speed(1) #Tiempo que tarda en dibujar en escala de 1: muy lento, 10 muy rápido
+estrella.left(180)
+for i in range(puntas): #Solo funciona para la estrella de 5 puntas
+    estrella.left(144)
+    estrella.forward(100)
+turtle.done()
